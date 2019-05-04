01810353bf4efc7a2b0b3f2a1ca42b414c9c533f
Krushna26nk <Krushnanikam26@gmail.com>
2019-05-04 16:06:48 +0530

    Changes commited

---
+++ OOP/Objectoriented/src/JsonInventoryManagement.java
@@ -0,0 +1,78 @@
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.FileWriter;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+public class JsonInventoryManagement {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		// First Emp Object
+		
+		JSONObject obj1 = new JSONObject();
+		
+		obj1.put("Name", "Krushna");
+		obj1.put("Last Name", "Nikam");
+		obj1.put("Add", "Auranagabd");
+		obj1.put("Company", "Cap");
+		
+		JSONObject firstEMP = new JSONObject();
+		firstEMP.put("FirstEMP", obj1);
+
+		// Second Emp Object
+		
+		JSONObject obj2 = new JSONObject();
+		
+		obj2.put("Name", "Prashant");
+		obj2.put("Last Name", "Banage");
+		obj2.put("Add", "Auranagabd");
+		obj2.put("Company", "Capgemini");
+		
+		JSONObject secondEMP = new JSONObject();
+		secondEMP.put("SecondEMP",obj2);
+		
+		// third EMP obj creation
+		
+		JSONObject obj3 = new JSONObject();
+		
+		obj3.put("Name", "Gopal");
+		obj3.put("Last Name", "Kulkarni");
+		obj3.put("Add", "jalna");
+		obj3.put("Company", "Syntel");
+		
+		JSONObject thirdEMP = new JSONObject();
+		thirdEMP.put("Third EMP", obj3);
+		
+		// json aaray for listing all data
+		
+		JSONArray jsonList = new JSONArray();
+		
+		jsonList.add(firstEMP);
+		jsonList.add(secondEMP);
+		jsonList.add(thirdEMP);
+		
+		try (FileWriter file = new FileWriter("empolyees.json")) {
+			
+			file.write(jsonList.toJSONString());
+			file.flush();
+			
+		}
+		catch(Exception e){
+			System.out.println("Error :- " +e);
+		}
+//		JSONObject obj = new JSONObject();
+//		obj.put("name","Krushna");
+//		obj.put("Contact", "9028960981");
+//		obj.put("Address","Aurangabad");
+//		
+//		StringWriter stringWrite = new StringWriter();
+//		
+//		obj.writeJSONString(stringWrite);
+//		
+//		String JsonText = stringWrite.toString();
+//		System.out.print(JsonText);
+	}
+
+}
---
+++ OOP/Objectoriented/src/org/json/simple/ReadInventoryJson.java
@@ -0,0 +1,71 @@
+package org.json.simple;
+
+import java.io.FileReader;
+import  java.io.IOException;
+import java.util.Iterator;
+import java.io.FileNotFoundException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class ReadInventoryJson {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// JSON parser object to parse read file.
+		JSONParser parser = new JSONParser();
+		
+		try(FileReader fileReader = new FileReader("inventory.json")) {
+			
+			// read the file using parser object.
+			
+			Object obj = parser.parse(fileReader);
+			JSONArray empList = new JSONArray();
+			empList.add(obj);
+			
+			System.out.println(empList);
+			
+			Iterator<String> itr = empList.iterator();
+			while(itr.hasNext()) {
+				JSONObject data = null;
+				parseEmployeeObject((JSONObject)data);
+			}
+			// iterate over the array
+			
+//			empList.forEach(data -> parseEmployeeObject( (JSONObject) data));
+			
+		} catch (FileNotFoundException e) {
+           System.out.println(e);
+        } catch (IOException e) {
+        	System.out.println(e);
+        } catch (ParseException e) {
+        	System.out.println(e);
+        }
+		
+	}
+	
+	private static void parseEmployeeObject(JSONObject data) {
+		
+		// get employee object within list
+		JSONObject empObject = (JSONObject) data.get("Rice");
+		System.out.println();
+		
+		// get employee first name
+		String firstName = (String) empObject.get("Rice Name");
+		System.out.println(firstName);
+		
+		String lastName = (String) empObject.get("Price");
+		System.out.println(lastName);
+//		
+//		String add = (String) empObject.get("Add");
+//		System.out.println(add);
+//		
+		System.out.println();
+		
+		
+	}
+
+}
